8e03fbc6e3d9f457cf98da03dd97a86731332ba7
Porter Morrison <24portermorrison@usd266.com>
2023-12-04 10:29:43 -0600

    did work

---
+++ 06ListsAndLoops/Assignment/main.py
@@ -0,0 +1,37 @@
+# def count_failing_grades(grades):
+#     num_of_failing = 0
+#     for grade in grades:
+#         if grade < 60:
+#             num_of_failing =  num_of_failing + 1
+#     return num_of_failing
+
+# print(count_failing_grades([50,30,80]))
+# COMPLETE
+
+
+# def count_act_scores(scores):
+#     valid_score_count = 0
+#     for score in scores:
+#         if score >0 and score <37:
+#             valid_score_count = valid_score_count + 1
+#     return valid_score_count
+# print(count_act_scores([300,21,0,58,30]))
+# #Complete
+
+
+# def number_sum(nums):
+#     total = 0
+#     for num in nums:
+#         total = total + num
+#     return total
+
+# print(number_sum([1, 1, 5, 2]))
+
+# complete
+
+
+def average_act_score(nums):
+    valid_count = 0
+    for num in nums:
+        if num < 37 and num > 0:
+            valid_count = valid_count + 1
