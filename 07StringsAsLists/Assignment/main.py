f6f09f1d7cc1ea9d50f1995c8a0f7396165ca25c
Porter Morrison <24portermorrison@usd266.com>
2023-12-11 10:28:39 -0600

    did workk

--- 07StringsAsLists/Assignment/main.py
+++ 07StringsAsLists/Assignment/main.py
@@ -8,12 +8,40 @@
 
 # print(is_alliteration("parry", "platapus"))
 # #Complete
-def count_vowels(word):
-    vowel_count = 0
-    for vowels in word:
-       if vowels == "a" or "e"or "i" or "o" or "u":
-           vowel_count = vowel_count + 1
-    return vowel_count
 
-print(count_vowels())
 
+# def count_vowels(word):
+#     vowel_count = 0
+#     for letters in word:
+#        if letters == "a" or letters == "e"or letters == "i" or letters == "o" or letters == "u":
+#            vowel_count = vowel_count + 1
+#     return vowel_count
+
+# print(count_vowels("bob"))
+# #Complete
+
+
+# def count_numbers(word):
+#     num_count = 0
+#     for letter in word:
+#         if letter in ["1","2","3","4","5","6","7","8","9","0"]:
+#             num_count = num_count +1
+#     return num_count
+# print(count_numbers("meo0w1ng"))
+# #complete
+
+
+# def count_target_letters(word,target):
+#     count = 0
+#     for letter in word:
+#         if letter == target:
+#             count = count + 1
+#     return count
+# print(count_target_letters("meow", "o"))
+# #complete
+
+
+def in_alphabetical_order(word):
+    previous = "a" 
+    for letter in word:
+        if letter > previous
